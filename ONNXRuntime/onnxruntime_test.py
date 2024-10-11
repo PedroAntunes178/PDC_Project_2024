a9d964d58eced0b9f772181c33f2ace66e07e1ed
Pedro Antunes <54639637+PedroAntunes178@users.noreply.github.com>
2024-10-11 17:59:28 +0200

    Update onnxruntime_test.py

--- ONNXRuntime/onnxruntime_test.py
+++ ONNXRuntime/onnxruntime_test.py
@@ -34,8 +34,8 @@ output = session.run([], {input_name: input_data})[0]
 pos_inference_time = time.time()
 inference_time = pos_inference_time-pre_inference_time
 
-result = ((output < expected_output*1.05) & (output > expected_output*0.95))
-print(result)
+result = ((output < expected_output*1.1) & (output > expected_output*0.9))
+print(result.all())
 if (not result.all()):
     print("Inference output is not what was expected.")
     print(f"Expected output: {expected_output}")
